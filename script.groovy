33fbd4a34d4cecfca8b586652d958a250282d229
Ashref Gamoudi <58528081+AshrefG@users.noreply.github.com>
2025-04-24 22:35:52 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t ashrefg/project_pipeline:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh '''echo "$PASS" | docker login -u "$USER" --password-stdin'''
         sh 'docker push ashrefg/project_pipeline:jma-2.0'
     }
 } 
